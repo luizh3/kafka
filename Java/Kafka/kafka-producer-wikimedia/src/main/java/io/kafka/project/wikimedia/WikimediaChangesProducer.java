bba8dbebc049086b3c45af8c68f1f00c8a90a9c1
luizhs331 <luizexhs@gmail.com.br>
2024-01-29 19:34:19 -0300

    feat: add OpenSeachConsumer and update WikiMediaChangesProducer

--- Java/Kafka/kafka-producer-wikimedia/src/main/java/io/kafka/project/wikimedia/WikimediaChangesProducer.java
+++ Java/Kafka/kafka-producer-wikimedia/src/main/java/io/kafka/project/wikimedia/WikimediaChangesProducer.java
@@ -25,9 +25,14 @@ public class WikimediaChangesProducer {
         properties.setProperty( ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName() );
 
         // Set safe producer configs ( Kafka <= 2.8 )
-//        properties.setProperty( ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true" );
-//        properties.setProperty( ProducerConfig.ACKS_CONFIG, "all"); // same as setting 1
-//        properties.setProperty( ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE ) );
+        properties.setProperty( ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true" );
+        properties.setProperty( ProducerConfig.ACKS_CONFIG, "all"); // same as setting 1
+        properties.setProperty( ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE ) );
+
+        // Set high throughput producer configs
+        properties.setProperty( ProducerConfig.LINGER_MS_CONFIG, "20" );
+        properties.setProperty( ProducerConfig.BATCH_SIZE_CONFIG, Integer.toString( 32 * 1024 ) );
+        properties.setProperty( ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy" );
 
         // Create the Producer
         KafkaProducer<String,String> producer = new KafkaProducer<>( properties );
